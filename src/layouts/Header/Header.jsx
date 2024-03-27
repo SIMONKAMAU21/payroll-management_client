dea8c1896750c7707715f967bad54420e9d81d1d
simon kamau <simogatuma21@gmail.com>
2024-03-27 08:45:27 +0300

    added some functional on employee dashbord

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -3,11 +3,14 @@ import './Header.scss';
 import { RiLogoutBoxRFill } from "react-icons/ri";
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-
+import UpdateEmployee from '../../components/Editprofile/editProfile';
+import { LoadingToast,SuccessToast,ErrorToast } from '../../components/toaster/Toaster';
 
 const Header = () => {
   const navigate = useNavigate();
   const [employeeData, setEmployeeData] = useState(null);
+  const [isUpdateProfileOpen, setIsUpdateProfileOpen] = useState(false);
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
 
   useEffect(() => {
     const userDetailsString = localStorage.getItem('userDetails');
@@ -18,32 +21,43 @@ const Header = () => {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('userDetailes')
+    localStorage.removeItem('userDetails');
     navigate('/');
   };
 
+  const handleProfileClick = () => {
+    setIsUpdateProfileOpen(true);
+  };
 
+  const handleCloseUpdateProfile = () => {
+    setIsUpdateProfileOpen(false);
+    setShowSuccessToast(true);
+  };
+
+  const handleToastClose = () => {
+    setShowSuccessToast(false);
+  };
 
   return (
     <div className='container'>
       <div className="logo">
         <div className="text">
-<img src="" alt="nolog" srcSet="no logo" />        </div>
+          <img src="" alt="nolog" srcSet="no logo" />
+        </div>
       </div>
       <div className="profile">
         <div className="search">
           <input type="text" placeholder='search...' />
         </div>
-        <div className="images">
+        <div className="images" onClick={handleProfileClick}>
           {employeeData && <img src={employeeData.PhotoURL} alt="Profile" />}
         </div>
-        <div className='logout'> 
+        <div className='logout'>
           <RiLogoutBoxRFill size='30px' color='aliceblue' onClick={handleLogout} />
         </div>
-      
-        </div>
-        
       </div>
+      {isUpdateProfileOpen && <UpdateEmployee onClose={handleCloseUpdateProfile} />}
+    </div>
   );
 };
 
